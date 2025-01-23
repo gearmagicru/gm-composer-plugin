9f21b893e17b25e832151d6267ca52e0ca7da0ce
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-01-23 16:02:29 +0300

    Update Installer.php

--- src/Installer.php
+++ src/Installer.php
@@ -1,27 +1,25 @@
 <?php
-
 namespace Gm\ComposerPlugin;
 
-use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
 
-abstract class Installer extends LibraryInstaller
+class Installer extends LibraryInstaller
 {
+    /**
+     * {@inheritdoc}
+     */
     public function supports($packageType)
     {
         return $packageType === 'gm-component';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getInstallPath(PackageInterface $package)
     {
         $name = explode("/", $package->getName());
         return "modules/{$name[1]}TTTTTTT/";
-    }
-
-    /**
-     * Implementing the abstract method test()
-     */
-    public function deactivate(){
-        //do something
     }
 }

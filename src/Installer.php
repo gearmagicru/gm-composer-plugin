aba8b4b0e66dff144d6d3abeb24efb0b306257a1
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-01-23 12:52:53 +0300

    Add files via upload

---
+++ src/Installer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * GM Installer.
+ * 
+ * @link https://gearmagic.ru/
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+namespace Gm\ComposerPlugin;
+
+use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+
+class Installer implements LibraryInstaller
+{
+    public function supports($packageType)
+    {
+        return $packageType === 'gm-component';
+    }
+/*
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        parent::install($repo, $package);
+
+        echo '---------------------------------';
+        $name = explode("/", $package->getName());
+        $this->initInstaller();
+    }
+
+    public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        $name = explode("/", $package->getName());
+        $this->initInstaller();
+
+        parent::uninstall($repo, $package);
+    }
+*/
+    public function getInstallPath(PackageInterface $package)
+    {
+        $name = explode("/", $package->getName());
+        return "modules/{$name[1]}/";
+    }
+}
---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * GM Installer.
+ * 
+ * @link https://gearmagic.ru/
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+namespace Gm\ComposerPlugin;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $composer->getInstallationManager()->addInstaller(new Installer($io, $composer));
+    }
+}

b472d8bfd18ab3feb20c256d5e4f9b87232fc493
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-01-23 17:09:39 +0300

    Add files via upload

--- src/Installer.php
+++ src/Installer.php
@@ -4,21 +4,42 @@ namespace Gm\ComposerPlugin;
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
+
 class Installer extends LibraryInstaller
 {
+    protected string $packageType = '';
+
+    protected array $packageMap = [
+        'gm-component' => '/modules/gm/{name}'
+    ];
+
     /**
      * {@inheritdoc}
      */
-    public function supports($packageType)
+    public function supports(string $packageType)
     {
+        $this->packageType = $packageType;
         return $packageType === 'gm-component';
     }
 
     /**
      * {@inheritdoc}
+     *
+     * @return string
      */
     public function getInstallPath(PackageInterface $package)
     {
+        print_r($package->getExtra());
+        exit;
+        return "modules/";
+
+        $this->initializeVendorDir();
+
+        $basePath = ($this->vendorDir ? $this->vendorDir.'/' : '') . $package->getPrettyName();
+        $targetDir = $package->getTargetDir();
+
+        return $basePath . ($targetDir ? '/'.$targetDir : '');
+
         $name = explode("/", $package->getName());
         return "modules/{$name[1]}TTTTTTT/";
     }
--- src/Plugin.php
+++ src/Plugin.php
@@ -7,8 +7,37 @@ use Composer\Plugin\PluginInterface;
 
 class Plugin implements PluginInterface
 {
+    /**
+     * Apply plugin modifications to Composer
+     *
+     * @return void
+     */
     public function activate(Composer $composer, IOInterface $io)
     {
         $composer->getInstallationManager()->addInstaller(new Installer($io, $composer));
+    }
+
+    /**
+     * Remove any hooks from Composer
+     *
+     * This will be called when a plugin is deactivated before being
+     * uninstalled, but also before it gets upgraded to a new version
+     * so the old one can be deactivated and the new one activated.
+     *
+     * @return void
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    /**
+     * Prepare the plugin to be uninstalled
+     *
+     * This will be called after deactivate.
+     *
+     * @return void
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
     }
 }
